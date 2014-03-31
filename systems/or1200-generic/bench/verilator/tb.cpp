44b6b69656205ae4e54e414f81e1d05048db9bd6
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-03-31 21:39:24 +0300

    or1200-generic: use verilator_tb_utils
    This removes all non-or1200 specific code that is now
    implemented in verilator_tb_utils instead.
    
    An (almost) empty argument parser template is added,
    which only purpose right now is to initiate the child parser
    in verilator_tb_utils.
    
    A signal handler is also added to catch ctrl-c.

--- systems/or1200-generic/bench/verilator/tb.cpp
+++ systems/or1200-generic/bench/verilator/tb.cpp
@@ -12,10 +12,13 @@
  */
 
 #include <stdint.h>
-#include <elf-loader.h>
+#include <signal.h>
+#include <argp.h>
+#include <verilator_tb_utils.h>
 
 #include "Vorpsoc_top__Syms.h"
-#include "verilated_vcd_c.h"
+
+static bool done;
 
 #define NOP_NOP			0x0000      /* Normal nop instruction */
 #define NOP_EXIT		0x0001      /* End of simulation */
@@ -32,193 +35,80 @@
 
 #define RESET_TIME		2
 
-#define VCD_DEFAULT_NAME	"../sim.vcd"
+void INThandler(int signal)
+{
+	printf("\nCaught ctrl-c\n");
+	done = true;
+}
 
-void parseArgs(int argc, char **argv);
+static int parse_opt(int key, char *arg, struct argp_state *state)
+{
+	switch (key) {
+	case ARGP_KEY_INIT:
+		state->child_inputs[0] = state->input;
+		break;
+	// Add parsing of custom options here
+	}
 
-int timeout;
-char *elf_file_name;
-bool vcd_enabled;
-char *vcd_name;
+	return 0;
+}
 
-bool dump_all;
-unsigned long dump_start, dump_end;
+static int parse_args(int argc, char **argv, VerilatorTbUtils* tbUtils)
+{
+	struct argp_option options[] = {
+		// Add custom options here
+		{ 0 }
+	};
+	struct argp_child child_parsers[] = {
+		{ &verilator_tb_utils_argp, 0, "", 0 },
+		{ 0 }
+	};
+	struct argp argp = { options, parse_opt, 0, 0, child_parsers };
+
+	return argp_parse(&argp, argc, argv, 0, 0, tbUtils);
+}
 
 int main(int argc, char **argv, char **env)
 {
-	int i;
-	int t = 0;
-	int size;
-	uint8_t *bin_file;
-	bool dump = false;
-	VerilatedVcdC* tfp;
 	uint32_t insn = 0;
+	uint32_t ex_pc = 0;
 
 	Verilated::commandArgs(argc, argv);
 
 	Vorpsoc_top* top = new Vorpsoc_top;
+	VerilatorTbUtils* tbUtils =
+		new VerilatorTbUtils(top->v->wb_bfm_memory0->mem);
 
-	parseArgs(argc, argv);
+	parse_args(argc, argv, tbUtils);
 
-	if (elf_file_name) {
-		printf("Loading %s\n",elf_file_name);
-		bin_file = load_elf_file(elf_file_name, &size);
-		if (bin_file == NULL) {
-			printf("Error loading elf file\n");
-			exit(1);
-		}
-		for (int i = 0; i < size; i += 4)
-			top->v->wb_bfm_memory0->mem[i / 4] = read_32(bin_file, i);
-	}
+	signal(SIGINT, INThandler);
 
 	top->wb_clk_i = 0;
 	top->wb_rst_i = 1;
 
-	if (vcd_enabled) {
-		Verilated::traceEverOn(true);
-		tfp = new VerilatedVcdC;
-		top->trace (tfp, 99);
-		tfp->open(vcd_name);
-	}
+	top->trace(tbUtils->tfp, 99);
 
-	if (dump_all)
-		printf("VCD dump started (%u)\n", t);
-
-	while (1) {
-
-		if (vcd_enabled) {
-			if (dump_start &&  t >= dump_start) {
-				if (t == dump_start)
-					printf("VCD dump started (%u)\n", t);
-				dump = true;
-			}
-			if (dump_all)
-				dump = true;
-			if (!dump_all && !dump_start && dump_end)
-				dump = true;
-			if (dump_end && t >= dump_end) {
-				if (t == dump_end)
-					printf("VCD dump stopped (%u)\n", t);
-				dump = false;
-			}
-			if (dump)
-				tfp->dump(t);
-		}
+	while (tbUtils->doCycle() && !done) {
+		if (tbUtils->getTime() > RESET_TIME)
+			top->wb_rst_i = 0;
 
 		top->eval();
-
-		if (t > RESET_TIME)
-			top->wb_rst_i = 0;
 
 		top->wb_clk_i = !top->wb_clk_i;
 
 		insn = top->v->or1200_top0->or1200_cpu->or1200_ctrl->wb_insn;
+		ex_pc = top->v->or1200_top0->or1200_cpu->or1200_except->ex_pc;
 
 		if (insn == (0x15000000 | NOP_EXIT)) {
-			printf("Success! Got NOP_EXIT. Exiting (%u)\n", t);
-			break;
+			printf("Success! Got NOP_EXIT. Exiting (%lu)\n",
+			       tbUtils->getTime());
+			done = true;
 		}
-
-		if (timeout && t > timeout) {
-			printf("Timeout reached (%u)\n", t);
-			break;
-		}
-		t++;
 	}
 
-	tfp->close();
-	free(vcd_name);
+	printf("Simulation ended at PC = %08x (%lu)\n",
+	       ex_pc, tbUtils->getTime());
+
+	delete tbUtils;
 	exit(0);
-}
-
-void parseArgs(int argc, char **argv)
-{
-	int i = 1;
-	while (i < argc) {
-
-		if (strcmp(argv[i], "--timeout") == 0) {
-			timeout = strtod(argv[i], NULL);
-			i++;
-			continue;
-		}
-/*
-		if ((strcmp(argv[i], "-q") == 0) || (strcmp(argv[i], "--quiet") == 0)) {
-			quiet = true;
-			i++;
-			continue
-		}
-*/
-		if (strcmp(argv[i], "--or1k-elf-load") == 0) {
-			i++;
-			elf_file_name = argv[i++];
-			continue;
-		}
-
-		if ((strcmp(argv[i], "-d") == 0) || (strcmp(argv[i], "--vcdfile") == 0) ||
-		    (strcmp(argv[i], "-v") == 0) || (strcmp(argv[i], "--vcd") == 0)) {
-			vcd_enabled = true;
-			dump_all = true;
-			if ((i + 1 < argc) && (argv[i + 1][0] != '-')) {
-					vcd_name = strdup(argv[i + 1]);
-					i++;
-			} else
-				vcd_name = strdup(VCD_DEFAULT_NAME);
-			i++;
-			continue;
-		}
-
-		if ((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--vcdstart") == 0)) {
-			dump_start = strtod(argv[i + 1], NULL);
-			dump_all = false;
-			i++;
-			continue;
-		}
-
-
-		if ((strcmp(argv[i], "-t") == 0) || (strcmp(argv[i], "--vcdstop") == 0)) {
-			dump_end = strtod(argv[i + 1], NULL);
-			dump_all = false;
-			i++;
-			continue;
-		}
-
-#ifdef JTAG_DEBUG
-		if ((strcmp(argv[i], "-r") == 0) || (strcmp(argv[i], "--rsp") == 0)) {
-			rsp_server_enabled = true;
-			if (i + 1 < argc)
-				if (argv[i + 1][0] != '-') {
-				rsp_server_port = atoi(argv[i + 1]);
-				i++;
-			}
-			i++;
-			continue;
-		}
-#endif
-
-		if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0)) {
-			printf("Usage: %s [options]\n", argv[0]);
-			printf("\n  ORPSoCv3 cycle accurate model\n");
-			printf("  For details visit http://opencores.org/openrisc,orpsocv2\n");
-			printf("\n");
-			printf("Options:\n");
-			printf("  -h, --help\t\tPrint this help message\n");
-			printf("  -q, --quiet\t\tDisable all except UART print out\n");
-			printf("\nSimulation control:\n");
-			printf("  -f, --program <file> \tLoad program from OR32 ELF <file>\n");
-			printf("  --timeout <val> \tStop the sim at <val> ns\n");
-			printf("\nVCD generation:\n");
-			printf("  -v, --vcdon\t\tEnable VCD generation\n");
-			printf("  -d, --vcdfile <file>\tEnable and save VCD to <file>\n");
-			printf("  -s, --vcdstart <val>\tEnable and delay VCD generation until <val> ns\n");
-			printf("  -t, --vcdstop <val> \tEnable and terminate VCD generation at <val> ns\n");
-#ifdef JTAG_DEBUG
-			printf("\nRemote debugging:\n");
-			printf("  -r, --rsp [<port>]\tEnable RSP debugging server, opt. specify <port>\n");
-#endif
-			printf("\n");
-			exit(0);
-		}
-
-		i++;
-	}
 }
