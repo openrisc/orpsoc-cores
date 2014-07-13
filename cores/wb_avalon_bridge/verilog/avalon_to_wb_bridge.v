967f200a6798ec421e3b0e46996a71ea1fc06d3e
Christian Svensson <blue@cmd.nu>
2014-07-13 02:09:50 +0200

    Update wb_avalon_bridge for wb_intercon_gen
    Renames port on wb_avalon_bridge to match those generated in
    wb_intercon_gen. This also changes the view of the port to be an "input
    master" to a "slave" port, which is functionally equivalent.

--- cores/wb_avalon_bridge/verilog/avalon_to_wb_bridge.v
+++ cores/wb_avalon_bridge/verilog/avalon_to_wb_bridge.v
@@ -27,18 +27,18 @@ module avalon_to_wb_bridge #(
 	parameter DW = 32,	// Data width
 	parameter AW = 32	// Address width
 )(
-	input 		  clk,
-	input 		  rst,
-	// Avalon Master input
-	input [AW-1:0] 	  avm_address_i,
-	input [DW/8-1:0]  avm_byteenable_i,
-	input 		  avm_read_i,
-	output [DW-1:0]   avm_readdata_o,
-	input [7:0] 	  avm_burstcount_i,
-	input 		  avm_write_i,
-	input [DW-1:0] 	  avm_writedata_i,
-	output 		  avm_waitrequest_o,
-	output 		  avm_readdatavalid_o,
+	input 		  wb_clk_i,
+	input 		  wb_rst_i,
+	// Avalon Slave input
+	input [AW-1:0] 	  s_av_address_i,
+	input [DW/8-1:0]  s_av_byteenable_i,
+	input 		  s_av_read_i,
+	output [DW-1:0]   s_av_readdata_o,
+	input [7:0] 	  s_av_burstcount_i,
+	input 		  s_av_write_i,
+	input [DW-1:0] 	  s_av_writedata_i,
+	output 		  s_av_waitrequest_o,
+	output 		  s_av_readdatavalid_o,
 	// Wishbone Master Output
 	output [AW-1:0]   wbm_adr_o,
 	output [DW-1:0]   wbm_dat_o,
@@ -56,31 +56,31 @@ module avalon_to_wb_bridge #(
 
 reg read_access;
 
-always @(posedge clk)
-	if (rst)
+always @(posedge wb_clk_i)
+	if (wb_rst_i)
 		read_access <= 0;
 	else if (wbm_ack_i | wbm_err_i)
 		read_access <= 0;
-	else if (avm_read_i)
+	else if (s_av_read_i)
 		read_access <= 1;
 
 reg readdatavalid;
 reg [DW-1:0] readdata;
-always @(posedge clk) begin
+always @(posedge wb_clk_i) begin
 	readdatavalid <= (wbm_ack_i | wbm_err_i) & read_access;
 	readdata <= wbm_dat_i;
 end
 
-assign wbm_adr_o = avm_address_i;
-assign wbm_dat_o = avm_writedata_i;
-assign wbm_sel_o =  avm_byteenable_i;
-assign wbm_we_o = avm_write_i;
-assign wbm_cyc_o = read_access | avm_write_i;
-assign wbm_stb_o = read_access | avm_write_i;
+assign wbm_adr_o = s_av_address_i;
+assign wbm_dat_o = s_av_writedata_i;
+assign wbm_sel_o =  s_av_byteenable_i;
+assign wbm_we_o = s_av_write_i;
+assign wbm_cyc_o = read_access | s_av_write_i;
+assign wbm_stb_o = read_access | s_av_write_i;
 assign wbm_cti_o = 3'b111; // TODO: support burst accesses
 assign wbm_bte_o = 2'b00;
-assign avm_waitrequest_o = !(wbm_ack_i | wbm_err_i);
-assign avm_readdatavalid_o = readdatavalid;
-assign avm_readdata_o = readdata;
+assign s_av_waitrequest_o = !(wbm_ack_i | wbm_err_i);
+assign s_av_readdatavalid_o = readdatavalid;
+assign s_av_readdata_o = readdata;
 
 endmodule
