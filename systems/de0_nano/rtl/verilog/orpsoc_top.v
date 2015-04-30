ab56197fede78e2daddbca8334f0721f37107e97
Olof Kindgren <olof.kindgren@gmail.com>
2015-05-01 00:31:17 +0200

    de0_nano: Move Boot ROM to 0xf0000000 and increase size

--- systems/de0_nano/rtl/verilog/orpsoc_top.v
+++ systems/de0_nano/rtl/verilog/orpsoc_top.v
@@ -255,7 +255,7 @@ assign or1k_rst = wb_rst | or1k_dbg_rst;
 
 `ifdef OR1200
 
-or1200_top #(.boot_adr(32'hf0000100))
+or1200_top #(.boot_adr(32'hf0000000))
 or1200_top0 (
 	// Instruction bus, clocks, reset
 	.iwb_clk_i			(wb_clk),
@@ -351,7 +351,7 @@ mor1kx #(
 	.IBUS_WB_TYPE("B3_REGISTERED_FEEDBACK"),
 	.DBUS_WB_TYPE("B3_REGISTERED_FEEDBACK"),
 	.OPTION_CPU0("CAPPUCCINO"),
-	.OPTION_RESET_PC(32'hf0000100)
+	.OPTION_RESET_PC(32'hf0000000)
 ) mor1kx0 (
 	.iwbm_adr_o(wb_m2s_or1k_i_adr),
 	.iwbm_stb_o(wb_m2s_or1k_i_stb),
@@ -484,7 +484,7 @@ adbg_top dbg_if0 (
 //
 ////////////////////////////////////////////////////////////////////////
 
-   localparam WB_RAM_MEM_DEPTH = 128;
+   localparam WB_RAM_MEM_DEPTH = 1024;
    
 wb_ram
   #(.depth (WB_RAM_MEM_DEPTH),
