b1a036dc9114668321e71d6c19c368b5e90c8c5e
Olof Kindgren <olof.kindgren@gmail.com>
2016-09-12 21:36:03 +0200

    vlog_tb_utils: Fix tap generator to write test plan

--- cores/vlog_tb_utils/vlog_tap_generator.v
+++ cores/vlog_tb_utils/vlog_tap_generator.v
@@ -18,11 +18,13 @@
 
 module vlog_tap_generator
   #(parameter TAPFILE          = "",
+    parameter NUM_TESTS        = 0,
     parameter MAX_STRING_LEN   = 80,
     parameter MAX_FILENAME_LEN = 1024);
 
    integer f; //File handle
    integer cur_tc = 0; //Current testcase index
+   integer numtests = NUM_TESTS; //Total number of testcases
 
    reg [MAX_FILENAME_LEN*8-1:0] tapfile; //TAP file to write
 
@@ -42,6 +44,16 @@ module vlog_tap_generator
       end
    endtask
 
+   task set_numtests;
+      input integer i;
+      begin
+	 if (cur_tc)
+	   $display("Error: Can't change number of tests. Already started writing to %0s", tapfile);
+	 else
+	   numtests = i;
+      end
+   endtask
+
    task write_tc;
       input [MAX_STRING_LEN*8-1:0] description;
       input 			   ok_i;
@@ -49,8 +61,12 @@ module vlog_tap_generator
 	 if (f === 32'dx) begin
 	    if(tapfile == 0)
 	      $display("No TAP file specified");
-	    else
-	      f = $fopen(tapfile,"w");
+	    else if (numtests == 0)
+	      $display("Number of tests must be specified");
+	    else begin
+	       f = $fopen(tapfile,"w");
+	       $fwrite(f, "1..%0d\n", numtests);
+	    end
 	 end
 
 	 if (f) begin
